73b6325f44a333872c8fc0b56733ae9dfd152840
GYonan <g2yonan@gmail.com>
2023-04-18 17:49:56 -0500

    yur

--- NFA2DFA.java
+++ NFA2DFA.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class NFA2DFA {
     public static void main(String[] args) throws FileNotFoundException {
         File NFA = new File("x.nfa");
-        Parse.ParseStudentA(NFA);
+        ReturnStructure<Hashtable<Pair<Character, Character>, ArrayList<Integer>>, String, String[], String[]>  returnStruct = Parse.ParseStudentA(NFA);
+        System.out.println(returnStruct.getPairedHashTable());
     }
 }
