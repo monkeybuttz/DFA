c4f116a468b721e31bd590920a85b2bf90d09e36
kadenhargrove <kadenhargrove@gmail.com>
2023-04-21 15:21:45 -0500

    implement parse for student B

--- Parse.java
+++ Parse.java
@@ -67,4 +67,68 @@ public class Parse {
         ReturnStructure<Hashtable<Pair<Character, Character>, ArrayList<Integer>>, String, String[], ArrayList<String>> returnStructure = new ReturnStructure<>(transitionTable, startState, finalStatesArray, input);
         return returnStructure;
     }
+
+    public static ReturnStructure<Hashtable<Pair<Character, Character>, ArrayList<Integer>>, String, String[], ArrayList<String>> ParseStudentB(File DFA) throws FileNotFoundException {
+        Scanner scan = new Scanner(DFA);
+
+        // main data structure
+        // key: Pair<current_state, alphabet>
+        // value: ArrayList<transition_states>
+        Hashtable<Pair<Character, Character>, ArrayList<Integer>> transitionTable = new Hashtable<Pair<Character, Character>, ArrayList<Integer>>();
+
+        String num_of_states = scan.nextLine();
+        String[] stateArray = num_of_states.split(" ");
+        int states = Integer.parseInt(stateArray[stateArray.length - 1]);
+
+        String letters = scan.nextLine();
+        String[] secondLine = letters.split("['' ]+");
+        String[] alphabet = Arrays.copyOfRange(secondLine, 1, secondLine.length);
+        Character [] alphabetArray = new Character[alphabet.length];
+        for(int i = 0; i < alphabet.length; i++) {
+            alphabetArray[i] = alphabet[i].charAt(0);
+        }
+        scan.nextLine();
+
+        for(Integer i = 0; i < states; i++) {
+            String line = scan.nextLine();
+            String[] lineArray = line.split("['' ]+");
+            Character key = lineArray[1].charAt(0);
+            for(int j = 2; j < lineArray.length; j++) {
+                String value = lineArray[j];
+                //String[] values = value.split("");
+                ArrayList<Integer> nodes = new ArrayList<Integer>();
+                Pair<Character, Character> transitions = new Pair<Character, Character>(key, alphabetArray[j-2]);
+
+                if (Character.isDigit(value.charAt(0)))
+                {
+                    nodes.add(Integer.parseInt(value));
+                }
+
+                transitionTable.put(transitions, nodes);
+            }
+        }
+        scan.nextLine();
+
+        String startState = scan.nextLine();
+        String[] startStateArray = startState.split(" ");
+        startState = startStateArray[startStateArray.length - 1];
+
+        String finalStates = scan.nextLine();
+        String[] finalStatesArray = finalStates.split(" ");
+        finalStates = finalStatesArray[finalStatesArray.length - 1];
+        finalStatesArray = finalStates.split(",");
+
+        scan.nextLine();
+        scan.nextLine();
+        scan.nextLine();
+        ArrayList<String> input = new ArrayList<String>();
+        while(scan.hasNextLine()) {
+            String line = scan.nextLine();
+            input.add(line);
+        }
+        scan.close();
+
+        ReturnStructure<Hashtable<Pair<Character, Character>, ArrayList<Integer>>, String, String[], ArrayList<String>> returnStructure = new ReturnStructure<>(transitionTable, startState, finalStatesArray, input);
+        return returnStructure;
+    }
 }
--- minimizeDFA.java
+++ minimizeDFA.java
@@ -1,6 +1,23 @@
+// This program takes a DFA from a text file and minimizes it to an
+// optimal DFA. The optimal DFA gets written to a new text file.
+// By Kaden Hargrove
+// IT 328 Section 1
+// Programming Assignment 2 + 3
+// Student B
+// 4/14/23
+
 import java.util.*;
 import java.io.*;
 
 public class minimizeDFA {
-
+    //construct matrix
+    //determine indistinguishables
+    //combine indistinguishable states
+    //reconstruct DFA
+    //you win
+    public static void main(String[] args) throws FileNotFoundException {
+        File DFA = new File("X.dfa");
+        ReturnStructure<Hashtable<Pair<Character, Character>, ArrayList<Integer>>, String, String[], ArrayList<String>>  returnStruct = Parse.ParseStudentB(DFA);
+        returnStruct.printStructure();
+    }
 }
