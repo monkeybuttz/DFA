b9d02e24f38335db62ca71519fc38fc3c65cc89c
GYonan <g2yonan@gmail.com>
2023-04-29 22:31:57 -0500

    some methods

--- NFA2DFA.java
+++ NFA2DFA.java
@@ -11,14 +11,17 @@ public class NFA2DFA {
 
     // takes the NFA in the return structure and converts it to a DFA and writes it to a file
     public static void convertNFA2DFA(ReturnStructure<Hashtable<Pair<Character, Character>, ArrayList<Integer>>, String, String[], Character[], ArrayList<String>>  returnStruct) {
-        Hashtable<Pair<Character, Character>, ArrayList<Integer>> transitionTable = returnStruct.getPairedHashTable();
-        String startState = returnStruct.getStartState();
-        String[] finalStates = returnStruct.getAcceptingStates();
-        ArrayList<String> inputStrings = returnStruct.getInputStrings();
+        Hashtable<Pair<Character, Character>, ArrayList<Integer>> NFAtransitionTable = returnStruct.getPairedHashTable();
+        String NFAstartState = returnStruct.getStartState();
+        String[] NFAfinalStates = returnStruct.getAcceptingStates();
+        Character[] NFAalphabet = returnStruct.getAlphabetStrings();
+        Integer NFAstates = returnStruct.getNumStates();
+        System.out.println(NFAstates);
+        ArrayList<String> NFAinputStrings = returnStruct.getInputStrings();
         
         //algorithm to convert NFA transition table to DFA transition table
 
-
+    
 
 
     }
